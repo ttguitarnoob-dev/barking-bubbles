ab777c12b5df2828bf47aff1e0c5bbd169791558
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2026-04-30 12:02:24 +0000

    modified about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,12 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import { Button, Card } from "@heroui/react";
+import { useNavigate } from "react-router-dom";
 
 export default function AboutPage() {
+
+  const navigate = useNavigate()
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
@@ -15,11 +19,15 @@ export default function AboutPage() {
             />
             <Card.Content>
               <h3 className="text-xl font-bold text-accent">Hi I'm Hazel!</h3>
-              <p>I am 11 years old and I started this business because I love animals. I have one bunny, three gerbils, three cats, and five chickens. I enjoy hanging out with dogs (Even if it means getting soaked!) I am currently being trained by another fellow groomer, so my skills are always improving. Whether a tiny chihuahua or a Great Pyrenees, your dog will be showered in affection and comfort! I hope you will consider me as your dog groomer. If you want to schedule a booking, take a look at my schedule and figure out the time that works best for you. Feel free to contact me if you have any questions!
-              Thank you! </p>
+              <p>I am 11 years old and I started this business because I love animals. I have one bunny, three gerbils, three cats, and five chickens. I enjoy hanging out with dogs (Even if it means getting soaked!) I am currently being trained by another fellow groomer, so my skills are always improving. Whether a tiny chihuahua or a Great Pyrenees, your dog will be showered in affection and comfort! I hope you will consider me as your dog groomer. If you want to schedule a booking, take a look at my schedule and figure out the time that works best for you. If you have any questions, fill out the contact form and we'll get back to you!
+                Thank you! </p>
             </Card.Content>
             <Card.Footer className="flex justify-center">
-              <Button>Contact</Button>
+              <Button onPress={() => navigate("/contact")}>
+
+                Contact
+
+              </Button>
             </Card.Footer>
           </Card>
         </div>
